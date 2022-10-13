5384946031613712cf7412961fbc0e693a7189cc
realkevinbrian <realkevinbrian@gmail.com>
2022-10-13 09:17:01 +0200

    Added Footer Component Section

---
+++ src/Components/Footer/Styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const FooterWrapper = styled.div``;
+export const FooterAbout = styled.div``;
+export const FooterList = styled.div``;
+export const FooterPrivacy = styled.div``;
+export const FooterMedia = styled.div``;
---
+++ src/Components/Footer/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Logo } from "../Navbar/Styled";
+import {
+  FooterAbout,
+  FooterList,
+  FooterMedia,
+  FooterPrivacy,
+  FooterWrapper,
+} from "./Styled";
+import fb from "../../Assets/images/icon-facebook.svg";
+import tw from "../../Assets/images/icon-twitter.svg";
+import pint from "../../Assets/images/icon-pinterest.svg";
+import insta from "../../Assets/images/icon-instagram.svg";
+
+function index() {
+  //list link
+  const list = ["About", "careers", "event", "products", "support"].map(
+    (item) => (
+      <a href="/" key={item}>
+        {item}
+      </a>
+    )
+  );
+
+  //social media
+  const media = [fb, tw, pint, insta];
+
+  return (
+    <FooterWrapper>
+      <FooterAbout>
+        <Logo />
+        <FooterList>{list}</FooterList>
+      </FooterAbout>
+
+      <FooterPrivacy>
+        <FooterMedia>{media}</FooterMedia>
+        <p>
+          &copy; 2022 Loopstudios, developed by &heart;
+          <a href="realkevinbrian.netlify.app">Kevin Brian</a>All rights
+          reserved
+        </p>
+      </FooterPrivacy>
+    </FooterWrapper>
+  );
+}
+
+export default index;
