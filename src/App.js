585daf4530cbc4172b51034da03cf7bb097a38c3
realkevinbrian <realkevinbrian@gmail.com>
2022-10-13 09:16:53 +0200

    Added About Component Section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Hero from "./Components/Hero";
+import About from "./Components/About";
+import Blog from "./Components/Blog";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <Hero />
+      <About />
+      <Blog />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/About/Styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const AboutWrapper = styled.div``;
+export const AboutMediaWrapper = styled.div``;
+export const AboutContentWrapper = styled.div``;
+export const AboutContentText = styled.div``;
